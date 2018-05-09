5af147ed51ae9dba382e803854ee52fd594c7a03
Maxxel <bogaturev.maxim@gmail.com>
2018-05-09 17:19:47 +0300

    Add files via upload

---
+++ SortingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SortingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9da8f4f2-c8f1-4f4b-83a2-7b3efc2a6300")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SortingTests/Tests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortingTests
+{
+	[TestClass]
+	public class Tests
+	{
+		[TestMethod, Timeout(100)]
+		public void ShuffleDeckTest()
+		{
+			var beforeShuffle = SortingLib.SortingLib.CardsList;
+			SortingLib.SortingLib.Shuffle();
+			var afterShuffle = SortingLib.SortingLib.CardsList;
+
+			var isShuffled = false;
+			for (var i = 0; i < beforeShuffle.Count; i++)
+			{
+				if (beforeShuffle[i].StartPoint == afterShuffle[i].StartPoint) continue;
+				isShuffled = true;
+				return;
+			}
+			Assert.IsTrue(isShuffled, "The deck is not mixed");
+		}
+
+		[TestMethod, Timeout(100)]
+		public void SortDeckTest()
+		{
+			SortingLib.SortingLib.Shuffle();
+
+			SortingLib.SortingLib.Sort();
+
+			var sortedList = SortingLib.SortingLib.CardsList;
+
+			for (var i = 0; i < sortedList.Count; i++)
+			{
+				if ((i + 1) < sortedList.Count)
+				{
+					Assert.IsTrue(sortedList[i].FinishPoint == sortedList[i + 1].StartPoint, "Error in sorting the deck");
+				}
+			}
+		}
+
+		[TestMethod, Timeout(120000)]
+		public void LinearAlgorithmTest()
+		{
+			SortingLib.SortingLib.IsTestMode = true;
+			const string errorStr = "Error.The algorithm is not linear";
+
+			var time1 = GetAlgorithmRuntimeSec();
+			SortingLib.SortingLib.AddNew("Krasnoyarsk", "Abakan");
+			var time2 = GetAlgorithmRuntimeSec();
+
+			Assert.IsTrue((time2 - time1) == 1, errorStr);
+
+			SortingLib.SortingLib.AddNew("Abakan", "Irkutsk");
+			var time3 = GetAlgorithmRuntimeSec();
+
+			Assert.IsTrue((time3 - time2) == 1, errorStr);
+		}
+
+		private int GetAlgorithmRuntimeSec()
+		{
+			SortingLib.SortingLib.Shuffle();
+
+			var watch = Stopwatch.StartNew();
+			SortingLib.SortingLib.Sort();
+			watch.Stop();
+
+			return watch.Elapsed.Seconds;
+		}
+	}
+}
